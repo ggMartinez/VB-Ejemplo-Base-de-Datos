ec096f94fd9c29f49a7117152c5194f2cb1aa0e6
Gonzalo Martinez <gon.martinez@icloud.com>
2020-06-04 21:51:40 -0300

    Unico commit

--- WindowsApp12/Form1.Designer.vb
+++ WindowsApp12/Form1.Designer.vb
@@ -22,10 +22,203 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.lblId = New System.Windows.Forms.Label()
+        Me.lblApellido = New System.Windows.Forms.Label()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblMail = New System.Windows.Forms.Label()
+        Me.txtMail = New System.Windows.Forms.TextBox()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnListar = New System.Windows.Forms.Button()
+        Me.btnSiguiente = New System.Windows.Forms.Button()
+        Me.GrillaDatos = New System.Windows.Forms.DataGridView()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnContar = New System.Windows.Forms.Button()
+        CType(Me.GrillaDatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(85, 129)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Insertar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(86, 38)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(86, 64)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(100, 20)
+        Me.txtApellido.TabIndex = 2
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(86, 12)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(100, 20)
+        Me.txtId.TabIndex = 3
+        '
+        'lblId
+        '
+        Me.lblId.AutoSize = True
+        Me.lblId.Location = New System.Drawing.Point(13, 15)
+        Me.lblId.Name = "lblId"
+        Me.lblId.Size = New System.Drawing.Size(18, 13)
+        Me.lblId.TabIndex = 4
+        Me.lblId.Text = "ID"
+        '
+        'lblApellido
+        '
+        Me.lblApellido.AutoSize = True
+        Me.lblApellido.Location = New System.Drawing.Point(13, 67)
+        Me.lblApellido.Name = "lblApellido"
+        Me.lblApellido.Size = New System.Drawing.Size(44, 13)
+        Me.lblApellido.TabIndex = 5
+        Me.lblApellido.Text = "Apellido"
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(12, 41)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 6
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblMail
+        '
+        Me.lblMail.AutoSize = True
+        Me.lblMail.Location = New System.Drawing.Point(13, 94)
+        Me.lblMail.Name = "lblMail"
+        Me.lblMail.Size = New System.Drawing.Size(26, 13)
+        Me.lblMail.TabIndex = 8
+        Me.lblMail.Text = "Mail"
+        '
+        'txtMail
+        '
+        Me.txtMail.Location = New System.Drawing.Point(85, 91)
+        Me.txtMail.Name = "txtMail"
+        Me.txtMail.Size = New System.Drawing.Size(100, 20)
+        Me.txtMail.TabIndex = 7
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(85, 159)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(100, 23)
+        Me.btnModificar.TabIndex = 9
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(86, 189)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(99, 23)
+        Me.btnEliminar.TabIndex = 10
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnListar
+        '
+        Me.btnListar.Location = New System.Drawing.Point(86, 278)
+        Me.btnListar.Name = "btnListar"
+        Me.btnListar.Size = New System.Drawing.Size(99, 23)
+        Me.btnListar.TabIndex = 11
+        Me.btnListar.Text = "Listar"
+        Me.btnListar.UseVisualStyleBackColor = True
+        '
+        'btnSiguiente
+        '
+        Me.btnSiguiente.Location = New System.Drawing.Point(86, 249)
+        Me.btnSiguiente.Name = "btnSiguiente"
+        Me.btnSiguiente.Size = New System.Drawing.Size(99, 23)
+        Me.btnSiguiente.TabIndex = 12
+        Me.btnSiguiente.Text = "Siguiente"
+        Me.btnSiguiente.UseVisualStyleBackColor = True
+        '
+        'GrillaDatos
+        '
+        Me.GrillaDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GrillaDatos.Location = New System.Drawing.Point(211, 12)
+        Me.GrillaDatos.Name = "GrillaDatos"
+        Me.GrillaDatos.Size = New System.Drawing.Size(272, 317)
+        Me.GrillaDatos.TabIndex = 13
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(85, 218)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(99, 23)
+        Me.btnBuscar.TabIndex = 14
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnContar
+        '
+        Me.btnContar.Location = New System.Drawing.Point(86, 306)
+        Me.btnContar.Name = "btnContar"
+        Me.btnContar.Size = New System.Drawing.Size(98, 23)
+        Me.btnContar.TabIndex = 15
+        Me.btnContar.Text = "Contar"
+        Me.btnContar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(495, 341)
+        Me.Controls.Add(Me.btnContar)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.GrillaDatos)
+        Me.Controls.Add(Me.btnSiguiente)
+        Me.Controls.Add(Me.btnListar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.lblMail)
+        Me.Controls.Add(Me.txtMail)
+        Me.Controls.Add(Me.lblNombre)
+        Me.Controls.Add(Me.lblApellido)
+        Me.Controls.Add(Me.lblId)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Apellido"
+        CType(Me.GrillaDatos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents lblId As Label
+    Friend WithEvents lblApellido As Label
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents lblMail As Label
+    Friend WithEvents txtMail As TextBox
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnListar As Button
+    Friend WithEvents btnSiguiente As Button
+    Friend WithEvents GrillaDatos As DataGridView
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnContar As Button
 End Class
--- WindowsApp12/Form1.vb
+++ WindowsApp12/Form1.vb
@@ -1,3 +1,115 @@
-﻿Public Class Form1
+﻿Imports System.Data.Odbc
 
+Public Class Form1
+
+    Private stringDeConexion As String = "DRIVER=MySQL ODBC 8.0 UNICODE Driver;UID=root;PWD=123;PORT=3307;DATABASE=prueba;SERVER=localhost"
+
+    Private lector As OdbcDataReader
+
+    Private Sub obtenerDatos()
+        Dim conexion As New OdbcConnection(stringDeConexion)
+        conexion.Open()
+
+        Dim comando As New OdbcCommand
+        comando.CommandText = "SELECT * FROM persona"
+
+        comando.Connection = conexion
+
+        Me.lector = comando.ExecuteReader()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim conexion As New OdbcConnection(stringDeConexion)
+        conexion.Open()
+
+        Dim comando As New OdbcCommand
+        If txtMail.Text = "" Then
+            comando.CommandText = "INSERT INTO persona(id,nombre,apellido) VALUES(" + txtId.Text + ",'" + txtNombre.Text + "','" + txtApellido.Text + "')"
+        Else
+            comando.CommandText = "INSERT INTO persona(id,nombre,apellido,mail) VALUES(" + txtId.Text + ",'" + txtNombre.Text + "','" + txtApellido.Text + "','" + txtMail.Text + "')"
+
+        End If
+        MsgBox(comando.CommandText)
+        comando.Connection = conexion
+
+        comando.ExecuteNonQuery()
+
+        MsgBox("Persona insertada correctamente")
+
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Dim conexion As New OdbcConnection(stringDeConexion)
+        conexion.Open()
+
+        Dim comando As New OdbcCommand
+
+        comando.CommandText = "UPDATE persona SET nombre = '" + txtNombre.Text + "', apellido = '" + txtApellido.Text + "', mail = '" + txtMail.Text + "' WHERE id = " + txtId.Text + ""
+
+        MsgBox(comando.CommandText)
+        comando.Connection = conexion
+
+        comando.ExecuteNonQuery()
+
+        MsgBox("Persona modificada correctamente")
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim conexion As New OdbcConnection(stringDeConexion)
+        conexion.Open()
+
+        Dim comando As New OdbcCommand
+
+        comando.CommandText = "DELETE FROM persona WHERE id = " + txtId.Text + ""
+
+        MsgBox(comando.CommandText)
+        comando.Connection = conexion
+
+        comando.ExecuteNonQuery()
+
+        MsgBox("Persona eliminada correctamente")
+    End Sub
+
+
+    Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        lector.Read()
+        txtId.Text = lector(0).ToString()
+        txtNombre.Text = lector(1).ToString()
+        txtApellido.Text = lector(2).ToString()
+        txtMail.Text = lector(3).ToString()
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        obtenerDatos()
+
+    End Sub
+
+    Private Sub btnListar_Click(sender As Object, e As EventArgs) Handles btnListar.Click
+        ' Llenamos el DataReader con el resultado de la query
+        obtenerDatos()
+
+        ' Definimos un DataTable (tabla en memoria)
+        Dim tabla As New DataTable
+
+        ' Llenamos el DataTable con el contenido del DataReader (resultado de la query)
+        tabla.Load(Me.lector)
+
+        ' Asginamos el contenido del DataTable al DataGridView para visualizarlo
+        GrillaDatos.DataSource = tabla
+    End Sub
+
+    Private Sub btnContar_Click(sender As Object, e As EventArgs) Handles btnContar.Click
+
+        Dim conexion As New OdbcConnection(stringDeConexion)
+            conexion.Open()
+
+            Dim comando As New OdbcCommand
+
+            comando.CommandText = "SELECT COUNT(*) FROM persona"
+            comando.Connection = conexion
+
+            MsgBox("Cantidad: " + comando.ExecuteScalar().ToString())
+
+
+    End Sub
 End Class
